71b841a26e52f0ff65012bc5b1c20305d75b2def
Matrocks84 <mathewdev84@gmail.com>
2026-09-26 01:56:15 +0300

    Add Contact us section

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker: lets the app open offline and load fast.
 // Change the version number whenever you update index.html so phones get the new version.
-const CACHE = "pricedrop-v4";
+const CACHE = "pricedrop-v5";
 const SHELL = ["./", "index.html", "manifest.json", "data.csv", "icons/icon-192.png", "icons/icon-512.png"];
 
 self.addEventListener("install", e => {
